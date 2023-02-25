6124db14afd313a82b409e4302967ea8c2a99faa
b.schlueter <schlueter.braden@gmail.com>
2023-02-25 15:59:59 -0600

    Current work is working. Need to implement the reading of the dataset file. Use the assignment1 & suggestedImplementation guide for a reference on what else i need.

--- Project Assignment 1/main.cpp
+++ Project Assignment 1/main.cpp
@@ -25,6 +25,17 @@ void displayMenu(){
     cout << "Enter your choice: ";
 }
 
+// Displays a header for the results table
+void displayHeader(){
+    cout << left 
+         << setw(7) << "BibNum"
+         << setw(18) << "Name"
+         << right
+         << setw(8) << "Distance"
+         << setw(10) << "Time"
+         << endl;
+}
+
 // Functions needed per the assignment instructions
 void readDataset(ifstream& in, Result results[], int &size); 
 void displayDataset(Result results[], int size); 
@@ -37,9 +48,40 @@ void sortByDistanceTime(Result results[], int size);
 
 int main(){
     int choice;
+
+    Result results[] = {  // This is testing data DELETE LATER
+                       {12, "TestingName", 12.5, "12:00:00"},
+                       {20, "Testing 2nd Name", 36, "9:30:00"},
+                       {1, "Testing 3rd Name", 1.5, "1:15:00"}
+    };
+    int count = 3;
     
     displayMenu();
-    cin >> choice;
-    // Validate the user's choice: DELTE THIS LATER
-    cout << choice << endl;
+        cin >> choice;
+    while (choice < 1 || choice > 5){ // Validate the user's choice
+        cout << "Invalid choice. Please enter a number between 1 and 5: ";
+            cin >> choice;
+    }
+    switch (choice){
+        case 1:
+            // display results sorted by bib number
+            break;
+        case 2:
+            // display results sorted by distance, then time
+            break;
+        case 3:
+            // lookup a bib number given a name
+            break;
+        case 4:
+            // lookup a result by bib number
+            break;
+        case 5:
+            // quit the program
+            cout << "Exiting program...";
+            return 0;
+            break;
+
+    }
+    
+
 }
