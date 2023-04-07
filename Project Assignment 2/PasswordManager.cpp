8a450e8b10bc318e1f453611b79d553e03ef4847
b.schlueter <schlueter.braden@gmail.com>
2023-04-07 14:31:27 -0500

    Defined all the member functions. Still need to code out the driver function

--- Project Assignment 2/PasswordManager.cpp
+++ Project Assignment 2/PasswordManager.cpp
@@ -6,6 +6,81 @@
 
 using namespace std;
 
+string PasswordManager::encrypt(string pass){
+    const int SHIFT = 37;         // amount to shift/cycle the ascii value
+    string encryptedStr;          // encrypted version of string
 
+    for (int i=0; i < pass.length(); i++) {
+        char x = ((encryptedStr[i]-33) + SHIFT) % 94 + 33;
+        pass.push_back(x);
+    }
+    return encryptedStr;
+}
+
+bool PasswordManager::meetsCriteria(string pass){
+    bool upper_count = 0, lower_count = 0, digit_count = 0, special_count = 0;
+    if (pass.length() < 15){
+        return false;
+    }
+    else{
+        for (int i = 0; i < pass.length(); i++){
+            if (isupper(pass[i])){
+                upper_count = 1;
+            }
+            else if (islower(pass[i])){
+                lower_count = 1;
+            }
+            else if (isdigit(pass[i])){
+                digit_count = 1;
+            }
+            else if (ispunct(pass[i])){
+                special_count = 1;
+            }
+        }
+        if (upper_count && lower_count && digit_count && special_count){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+
+void PasswordManager::setUsername(string user){
+    userName = user;
+}
+
+string PasswordManager::getUsername(){
+    return userName;
+}
+
+void PasswordManager::setEncryptedPassword(string pass){
+    encryptedPassword = pass;
+}
+
+string PasswordManager::getEncryptedPassword(){
+    return encryptedPassword;
+}
+
+bool PasswordManager::setNewPassword(string pass){
+    if (meetsCriteria(pass)){
+        encryptedPassword = encrypt(pass);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool PasswordManager::authenticate(string pass){
+    string tempPass = encrypt(pass);
+    if (tempPass == encryptedPassword){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 #endif
--- Project Assignment 2/PasswordManager.h
+++ Project Assignment 2/PasswordManager.h
@@ -3,15 +3,15 @@ using namespace std;
 
 class PasswordManager {
     private: 
-        string password;
+        string userName;
         string encryptedPassword;
-        string ecrypt(string pass);
+        string encrypt(string pass);
         bool meetsCriteria(string pass);
     public:
         void setUsername(string user);
         string getUsername();
         void setEncryptedPassword(string pass);
         string getEncryptedPassword();
-        void setNewPassword(string pass);
+        bool setNewPassword(string pass);
         bool authenticate(string pass);
 };
