be4b7c75b546735ee5cd604bcda0b3c8311aa636
b.schlueter <schlueter.braden@gmail.com>
2023-04-20 22:40:01 -0500

    added code comments

--- ICCP-04:21/LinkedList-Practice_Q1.cpp
+++ ICCP-04:21/LinkedList-Practice_Q1.cpp
@@ -10,10 +10,10 @@ class LinkedList{
         LinkedList() {head = nullptr;}
         void insertAfter(int target, int value){
             Node* temp = head;
-            while (temp != nullptr && temp->data != target){
+            while (temp != nullptr && temp->data != target){ // go through the list until the target is found
                 temp = temp->next;
             }
-            if (temp != nullptr){
+            if (temp != nullptr){ // if the target is found then insert the new node
                 Node* newNode = new Node();
                 newNode->data = value;
                 newNode->next = temp->next;
